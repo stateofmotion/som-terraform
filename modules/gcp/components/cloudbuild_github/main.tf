342ee6f1302e31aa5ee5dcd9c1a2625c59e4c82a
Jesse Belcourt <jessebelcourt@gmail.com>
2024-02-28 19:53:41 -0800

    Adds in functionality for creating cloud build repositories from the arguments passed to the cloudbuild_github module (#3)

--- modules/gcp/components/cloudbuild_github/main.tf
+++ modules/gcp/components/cloudbuild_github/main.tf
@@ -18,3 +18,13 @@ module "my_github_cloudbuild_connection" {
   region              = var.region
   source              = "../cloudbuild_github_connection"
 }
+
+module "repo_and_triggers" {
+  for_each            = var.repositories
+  project_id          = var.project_id
+  region              = var.region
+  source              = "../cloudbuild_repo_and_triggers"
+  parent_connection   = module.my_github_cloudbuild_connection.github_connection_id
+  repo_name           = each.key
+  remote_uri          = each.value["remote_uri"]
+}
