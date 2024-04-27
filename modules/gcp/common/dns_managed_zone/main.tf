d80c7fa94a1c916e834c2f4fa26d2fcf6ab4e3bc
jessebelcourt <jessebelcourt@gmail.com>
2024-04-26 17:29:12 -0700

    Update the managed zone to include a suffix due to naming conflict when destroying the zone and recreating it

--- modules/gcp/common/dns_managed_zone/main.tf
+++ modules/gcp/common/dns_managed_zone/main.tf
@@ -11,9 +11,14 @@ terraform {
   }
 }
 
+resource "random_string" "zone_suffix" {
+  length  = 6
+  special = false
+}
+
 resource "google_dns_managed_zone" "dns_zone" {
   project     = var.project_id
-  name        = var.name
+  name        = "${var.name}_${lower(random_string.zone_suffix.result)}"
   dns_name    = var.dns_name
   description = var.description
   dnssec_config {
