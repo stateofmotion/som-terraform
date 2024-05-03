4f78a96f753a7cd253a07aaae800f4859e3a7a22
jessebelcourt <jessebelcourt@gmail.com>
2024-05-03 12:58:41 -0700

    Update project Role to include the first index of the resource in the output since it is a conditional resource

--- modules/gcp/common/project_role/main.tf
+++ modules/gcp/common/project_role/main.tf
@@ -21,5 +21,5 @@ resource "google_project_iam_custom_role" "project_role" {
 }
 
 output "role_id" {
-  value = google_project_iam_custom_role.project_role.id
+  value = google_project_iam_custom_role.project_role[0].id
 }
